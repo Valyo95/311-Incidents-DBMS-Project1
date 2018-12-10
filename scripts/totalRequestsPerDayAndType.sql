1ca717cc74d6c9697c300cf5d10b688c6067efb3
IVANOV VALENTIN <valivan@intracom-telecom.com>
2018-12-10 16:55:16 +0200

    2nd procedure - totalRequestsPerDayAndType

---
+++ scripts/totalRequestsPerDayAndType.sql
@@ -0,0 +1,15 @@
+
+drop function if exists totalRequestsPerDay;
+create function totalRequestsPerDay(varchar,timestamp , timestamp )
+ RETURNS TABLE (
+ "day" date,
+ incidents bigINT
+) 
+AS $$
+select date(creation_date) as per_day, count(srn) from incident
+where service_request_type = $1
+and creation_date between $2 and $3
+group by per_day
+ $$ 
+LANGUAGE 'sql';
+
