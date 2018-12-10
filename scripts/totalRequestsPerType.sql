2863a8ad13c2b75ca3fc7decfbcc8fb0fd76c24d
IVANOV VALENTIN <valivan@intracom-telecom.com>
2018-12-10 16:54:49 +0200

    1st precedure - totalRequestsPerDay

---
+++ scripts/totalRequestsPerType.sql
@@ -0,0 +1,14 @@
+drop function if exists totalRequestsPerType;
+create function totalRequestsPerType(timestamp , timestamp )
+ RETURNS TABLE (
+ service_request_type VARCHAR,
+ incidents bigINT
+) 
+AS $$
+
+ select service_request_type, count(srn) as incidents from incident
+ where creation_date between $1 and $2
+ group by service_request_type
+ order by incidents desc
+ $$ 
+LANGUAGE 'sql';
