483153f9f6aeadd2acef8647107947476b35789b
IVANOV VALENTIN <valivan@intracom-telecom.com>
2018-12-12 17:08:03 +0200

    Added enpoints for procedures 1-11

--- src/main/java/com/incidents/repositories/RodentBaitingDAO.java
+++ src/main/java/com/incidents/repositories/RodentBaitingDAO.java
@@ -1,9 +1,22 @@
 package com.incidents.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.incidents.entities.RodentBaiting;
 
 
 public interface RodentBaitingDAO extends JpaRepository<RodentBaiting, Integer> {
 
+	@Query(value = "select * from rodentBaitingWithPremisesBaitedLessThan(?1)", nativeQuery = true)
+    List<Object> rodentBaitingWithPremisesBaitedLessThan(int number);
+
+	@Query(value = "select * from rodentBaitingWithGarbagePremisesBaitedLessThan(?1)", nativeQuery = true)
+    List<Object> rodentBaitingWithGarbagePremisesBaitedLessThan(int number);
+	
+	@Query(value = "select * from rodentBaitingWithRatsPremisesBaitedLessThan(?1)", nativeQuery = true)
+    List<Object> rodentBaitingWithRatsPremisesBaitedLessThan(int number);
+	
 }
